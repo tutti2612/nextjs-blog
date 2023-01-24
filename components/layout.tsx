0e57e372d7fa47e78e3999acaeec5062ffb23ea9
Kohei Tsuchiya <doyahirohira@gmail.com>
2023-01-24 23:30:26 +0900

    サンプル文言を自分用に変更

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,8 +4,8 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Your Name';
-export const siteTitle = 'Next.js Sample Website';
+const name = 'Kohei Tsuchiya';
+export const siteTitle = 'Kohei Tsuchiya Website';
 
 export default function Layout({
   children,
